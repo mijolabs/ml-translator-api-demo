2c4b46061ef947897cd96814f2a915624e3bde42
Michael Johansson <mijolabs@remotenode.io>
2023-03-04 07:06:02 +0100

    Fix relative path

--- src/dev-tools/start-server.py
+++ src/dev-tools/start-server.py
@@ -1,11 +1,15 @@
+from pathlib import Path
+
 import uvicorn
 
 
 
 if __name__ == "__main__":
+    app_dir = str(Path(__file__).parent.parent)
+
     uvicorn.run(
         "app.main:app",
-        app_dir="..",
+        app_dir=app_dir,
         host="0.0.0.0",
         port=8000,
         reload=True,
